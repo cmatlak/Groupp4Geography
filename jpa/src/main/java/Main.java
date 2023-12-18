113cf38fcf30a4538579ef76e1aef2a10e7b8598
Alfred <alfredbild@gmail.com>
2023-12-18 16:02:05 +0100

    Changes by Admin

--- jpa/src/main/java/Main.java
+++ jpa/src/main/java/Main.java
@@ -1,10 +1,8 @@
 import jakarta.persistence.*;
 
-import java.util.Currency;
 import java.util.List;
 import java.util.Scanner;
 import java.util.function.Consumer;
-
 public class Main {
 
     public static void main(String[] args) {
